e4755b5dad69dc5aa484055709641cd6a56ae8e4
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-03-20 18:35:49 -0700

    Update gc-priceEstimate.js

--- globecityresorthighland/scripts/gc-priceEstimate.js
+++ globecityresorthighland/scripts/gc-priceEstimate.js
@@ -1,9 +1,13 @@
 // Determining the estimated price of a ticket
 
-	//	Example use:
+	// Example use:
 	// prices = determinePrice(dateInput);
 	// adultPrice = prices[0];
 	// childPrice = prices[1];
+	// formatedDate = prices[3];
+
+	// ALL PRICES ARE BEFORE MULTI-DAY DISCOUNT
+	// example to fix: adultPrice = prices[0] - ((numDay-1)*multiDayDiscount);
 
 function determinePrice(dateInput) {
 	var priceAd; var priceChi;
